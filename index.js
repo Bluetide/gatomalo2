0ce2fc03fe60ea806132fcb81fbe1164aa1e601c
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-10-31 15:38:20 -0500

    added PUG suport

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const koa = require('koa')
 const app = koa()
+const pug = require('pug')
+
+const indexTemplate = pug.compileFile('./templates/index.pug')
 
 app.use(function*(){
-	this.body = "Hello World"
+	this.body = indexTemplate()
 })
 
 app.listen(5000)
