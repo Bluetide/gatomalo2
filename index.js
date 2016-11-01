3cebb0076076e870ed348dcf68eedfd124e9294f
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-11-01 10:47:39 -0500

    Added Frontend dependencies

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require('koa')()
 const Router = require('koa-router')
 const pug = require('koa-pug')
 const morgan = require('koa-morgan')
+const serve = require('koa-static')
+const mount = require('koa-mount')
 
 const pug_middleware = new pug({
 	viewPath: './templates',
@@ -25,6 +27,13 @@ RootRouter.get('/', function*(){
 app.use(morgan.middleware('combined'))
 app.use(RootRouter.routes())
 app.use(ApiRouter.routes())
-app.use(require('koa-static')('./public'))
+
+// Mount Static Dependencies and normalize addressses
+app.use(mount('/assets', serve(__dirname + '/public')))
+app.use(mount('/assets/javascripts', serve(__dirname + '/node_modules/jquery/dist')))
+app.use(mount('/assets/javascripts', serve(__dirname + '/node_modules/tether/dist/js')))
+app.use(mount('/assets/stylesheets', serve(__dirname + '/node_modules/tether/dist/css')))
+app.use(mount('/assets/javascripts', serve(__dirname + '/node_modules/bootstrap/dist/js')))
+app.use(mount('/assets/stylesheets', serve(__dirname + '/node_modules/bootstrap/dist/css')))
 
 app.listen(5000)
