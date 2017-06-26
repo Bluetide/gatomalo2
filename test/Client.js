90591158d71cea7c447a9436cfe003e7bddf37e1
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2017-06-26 11:57:58 -0500

    Adding CSS and improving typeface

--- test/Client.js
+++ test/Client.js
@@ -7,42 +7,42 @@ const {expect} = require('chai')
 
 describe('Client', function() {
 
-  describe('#string_output()', function() {
-    it('should output correctly', function() {
+	describe('#string_output()', function() {
+		it('should output correctly', function() {
 
-      let name = 'Lorem Ipsum'
-      let address = 'Second Star to the right'
-      let phone = '6666-6666'
-      let ruc = '23742364262348234'
-      let dv = '54'
+			let name = 'Lorem Ipsum'
+			let address = 'Second Star to the right'
+			let phone = '6666-6666'
+			let ruc = '23742364262348234'
+			let dv = '54'
 
-      let test_client = new Client(name, address, phone, ruc, dv)
-      let expected_result =
-        `jS${name}\n` +
-        `j1Direccion: ${address}\n` +
-        `j2Telefono: ${phone}\n` +
-        `jR${ruc}`
+			let test_client = new Client(name, address, phone, ruc, dv)
+			let expected_result =
+				`jS${name}\n` +
+				`j1Direccion: ${address}\n` +
+				`j2Telefono: ${phone}\n` +
+				`jR${ruc}`
 
-      expect(test_client.string_output()).to.equal(expected_result)
-    })
+			expect(test_client.string_output()).to.equal(expected_result)
+		})
 
-    it('should handle multiline addresses correctly', function(){
-      let name = 'Lorem Ipsum'
-      let address = 'Second\n Star\n to the right'
-      let phone = '6666-6666'
-      let ruc = '23742364262348234'
-      let dv = '54'
+		it('should handle multiline addresses correctly', function(){
+			let name = 'Lorem Ipsum'
+			let address = 'Second\n Star\n to the right'
+			let phone = '6666-6666'
+			let ruc = '23742364262348234'
+			let dv = '54'
 
 
-      let test_client = new Client(name, address, phone, ruc, dv)
+			let test_client = new Client(name, address, phone, ruc, dv)
 
-      let expected_result =
-        `jS${name}\n` +
-        `j1Direccion: Second, Star, to the right\n` +
-        `j2Telefono: ${phone}\n` +
-        `jR${ruc}`
+			let expected_result =
+				`jS${name}\n` +
+				`j1Direccion: Second, Star, to the right\n` +
+				`j2Telefono: ${phone}\n` +
+				`jR${ruc}`
 
-      expect(test_client.string_output()).to.equal(expected_result)
-    })
-  })
+			expect(test_client.string_output()).to.equal(expected_result)
+		})
+	})
 })
