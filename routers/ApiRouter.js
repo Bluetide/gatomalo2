e71b7e9c8bd101cc5fa9ee0643240a3b95f380a3
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2017-06-29 09:43:32 -0500

    Refactored ApiRouter. Removed unused route

--- routers/ApiRouter.js
+++ routers/ApiRouter.js
@@ -3,25 +3,20 @@
 const cloud_accounting = require('../cloud_accounting')
 const printer = require('../printer')
 const {Refund} = require('../model')
-const KoaBody = require('koa-body')()
-const ApiRouter = require('koa-router')({
-	prefix: '/api'
+const body = require('koa-body')
+const router = require('koa-router')({
+	prefix: '/api/invoice'
 })
 
-ApiRouter.get('invoice_page', '/:page', async function(ctx){
-	let page = ctx.params.page || 1
-	let response = await cloud_accounting.getInvoices(page)
-	ctx.body = JSON.parse(response.body)
-})
-
-ApiRouter.post('print_invoice', '/invoice/:invoice_id/print', async function(ctx){
+// Endopoint: Print Invoice
+router.post('print_invoice', '/:invoice_id/print', async (ctx) => {
 	let invoice = await cloud_accounting.getInvoice(ctx.params.invoice_id)
 	let result = await printer(invoice.string_output(), ctx.params.invoice_id)
 	ctx.body = invoice.string_output()
 })
 
-ApiRouter.post('refund_invoice', '/invoice/:invoice_id/refund',
-  KoaBody, async function(ctx){
+// Endpoint: Refund Invoice
+router.post('refund_invoice', '/:invoice_id/refund', body(), async (ctx) => {
 	let legacy_id = ctx.request.body.legacy_id
 	if (!legacy_id){
 		ctx.response.status = 400
@@ -35,10 +30,11 @@ ApiRouter.post('refund_invoice', '/invoice/:invoice_id/refund',
 	ctx.body = refund_model.string_output()
 })
 
-ApiRouter.post('test_invoice', '/invoice/:invoice_id/test', async function(ctx){
+// Endpoint: Print Test Invoice
+router.post('test_invoice', '/:invoice_id/test', async (ctx) => {
 	let invoice = await cloud_accounting.getInvoice(ctx.params.invoice_id)
 	let result = await printer(invoice.test_string_output())
 	ctx.body = result
 })
 
-module.exports = ApiRouter
+module.exports = router
