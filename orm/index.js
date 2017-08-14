92d580f07bd1a6be2c4f76fbb241c27ad908d4ab
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2017-08-14 11:45:03 -0500

    Docker-compose, attempt 1

--- orm/index.js
+++ orm/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('sqlite://database/gatomalo.sqlite')
+const sequelize = new Sequelize('postgres://postgres:bluetide@gatomalo2_database_1/gatomalo')
 const printed_invoice = sequelize.define('invoice', {
   zoho_id: Sequelize.STRING,
 })
