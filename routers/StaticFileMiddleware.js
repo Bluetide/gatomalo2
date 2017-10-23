649c904995fc709e68678152f624376984bc8509
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2017-10-23 10:29:42 -0500

    Updated packages. Bootstrap 4 beta 2.

--- routers/StaticFileMiddleware.js
+++ routers/StaticFileMiddleware.js
@@ -6,7 +6,6 @@ const compose = require('koa-compose')
 let mounts = [
 	mount('/assets/', serve('./public')),
 	mount('/assets/javascripts', serve('./node_modules/jquery/dist')),
-	mount('/assets/javascripts', serve('./node_modules/tether/dist/js')),
 	mount('/assets/javascripts', serve('./node_modules/bootstrap/dist/js')),
 ]
 
